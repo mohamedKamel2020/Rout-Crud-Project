3cf7add46cb7f64eb1198a31114e9363f344ef73
mohamedKamel2020 <m.kamelmohamed2020@gmail.com>
2024-12-23 20:48:01 +0200

    add Project  DAL

---
+++ Demo.DAL/Contexts/MVCAppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.DAL.Contexts
+{
+	public class MVCAppDBContext:IdentityDbContext<ApplicationUser>
+	{
+        public MVCAppDBContext(DbContextOptions<MVCAppDBContext> options):base(options)
+        {
+            
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+      	//optionsBuilder.UseSqlServer("Server=.;Database=MVCG02DB;Trusted_Connection = true;");
+		public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+      //  public DbSet<IdentityUser> Users { get; set; }
+	}
+}
